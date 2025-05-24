b69b21852657268d381c5c7b50edd6bf24aad957
Лужняк Александр <Lyzhniak2005@yandex.ru>
2025-05-24 16:52:12 +0300

    Gem for integrals+ frontend with it

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -3,9 +3,25 @@ class GraphsController < ApplicationController
   end
 
   def calculate
-    coefficients = params[:coefficients].split(',').map(&:to_f)
+
+    # Создаем полином и вычисляем интегралы
+    coefficients = params[:coefficients].split(',').map do |c|
+      Float(c.strip) rescue nil
+    end.compact
+
+    raise ArgumentError, "Введите хотя бы один коэффициент" if coefficients.empty?
+    raise ArgumentError, "Недопустимые символы в коэффициентах" if coefficients.any?(&:nil?)
+
+
+
     x_range = (-10.0..10.0).step(0.5).to_a
 
+    # Рассчитываем интегралы
+    poly = PolynomialCalculus::Polynomial.new(coefficients)
+    indefinite_integral = poly.indefinite_integral
+    definite_value = poly.definite_integral(1, 5) # Можно сделать параметры настраиваемыми
+
+    # Готовим данные для графика
     points = x_range.map do |x|
       y = coefficients.each_with_index.sum do |coeff, index|
         coeff * x**(coefficients.size - index - 1)
@@ -13,6 +29,10 @@ class GraphsController < ApplicationController
       { x: x, y: y }
     end
 
-    render json: { points: points }
+    render json: {
+      points: points,
+      integral: indefinite_integral.to_s,
+      definite: definite_value.round(4)
+    }
   end
 end
